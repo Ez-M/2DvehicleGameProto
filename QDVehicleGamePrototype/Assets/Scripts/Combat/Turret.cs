a44c58d503e202dd032a0c938c8221055badd725
Ezequiel <martinez.ezequiel42@gmail.com>
2023-06-08 15:23:03 -0700

    extracted a number of methods in the Turret class into their own static classes

--- QDVehicleGamePrototype/Assets/Scripts/Combat/Turret.cs
+++ QDVehicleGamePrototype/Assets/Scripts/Combat/Turret.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static AutoTurretAimStuff;
+using static AttackStuff;
 
 public class Turret : MonoBehaviour
 {
@@ -18,7 +20,6 @@ public class Turret : MonoBehaviour
     public float initialRotation;
     public float gunWeight;
     public int pivotLimit;
-    public float weaponRange;
     public WeaponData weaponData;
     public float nextShotTime;
 
@@ -61,84 +62,14 @@ public class Turret : MonoBehaviour
             }
             else
             {
-                Vector3? autoAimTarget = CalculateAutoAimTarget();
+                Vector3? autoAimTarget = CalculateAutoAimTarget(_turret: this);
                 if(autoAimTarget != null){
                     turretPointer.PointGun((Vector3)autoAimTarget, gunWeight, pivotLimit, initialRotation);}
             }
         }
     }
 
-    public Vector3? CalculateAutoAimTarget()
-    {
-        List<Vehicle> _enemyVehicles = owner.CalculateEnemyVehicles();
-        List<GameObject> _enemyObjects = new List<GameObject>();
-        foreach (Vehicle vehicle in _enemyVehicles)
-        { _enemyObjects.Add(vehicle.gameObject); }
 
-        List<GameObject> _validTargetsByAngle = ValidTargetsByAngle(_enemyObjects);
-
-        GameObject _closestVehicle = ClosestGameObject(_validTargetsByAngle);
-        if(_closestVehicle != null)
-        {
-            return _closestVehicle.transform.position;
-        } else {return null;}
-        
-    }
-
-    public List<GameObject> ValidTargetsByAngle(List<GameObject> _objectsList)
-    {
-        List<GameObject> _outputList = new List<GameObject>();
-
-        foreach (GameObject _currentObject in _objectsList)
-        {
-            Vector3 _direction = (_currentObject.transform.position - gunPivot.transform.position).normalized;
-            float targetAngle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
-
-            // Adjust the target angle based on the initial rotation
-            float adjustedTargetAngle = targetAngle - initialRotation;
-
-            // Normalize the adjusted target angle to be within the range of -180 to 180 degrees
-            if (adjustedTargetAngle > 180f)
-                adjustedTargetAngle -= 360f;
-            else if (adjustedTargetAngle < -180f)
-                adjustedTargetAngle += 360f;
-
-
-
-            if (adjustedTargetAngle < pivotLimit && adjustedTargetAngle > -pivotLimit)
-            {
-                _outputList.Add(_currentObject);
-            }
-        }
-
-
-        return _outputList;
-    }
-
-    public GameObject ClosestGameObject(List<GameObject> _ListObjects)
-    { //this should probably go somewhere else to be honest
-
-#nullable enable
-        GameObject? closestObject = null;
-#nullable disable
-        foreach (var _gameObject in _ListObjects)
-        {
-            var distanceFromObject = Vector3.Distance(this.gameObject.transform.position, _gameObject.transform.position);
-            if (closestObject == null || distanceFromObject < Vector3.Distance(this.transform.position, closestObject.transform.position))
-            {
-                closestObject = _gameObject;
-            }
-        }
-        if (closestObject != null)
-        {
-            return closestObject;
-        }
-        else
-        {
-            return null;
-        }
-
-    }
 
 
     public void FireGun()
@@ -180,53 +111,69 @@ public class Turret : MonoBehaviour
         }
     }
 
+    public void SetPlayerControlled(bool _setTo)
+    {
+        if(_setTo != isPlayerControlled)
+        {
+            isPlayerControlled = _setTo;
+            UpdateTurretInputSub();
+        }
+    }
+
+    private void UpdateTurretInputSub()
+    {
+        if (isPlayerControlled)
+        { playerManager.InputManger.PlayerShoot += FireGun; }
+        else { playerManager.InputManger.PlayerShoot -= FireGun; }
+    }
+}
+
+public static class AttackStuff
+{
+
+
 
 
 
     #region  attackTypeMethods
-    void hitscanAttack(Turret attackSource)
+    public static void hitscanAttack(Turret attackSource)
     {
 
-        // var weaponData = attackSource.weaponData;
-        // var gunPivot = attackSource.gunPivot;
-        // var lineRenderer = attackSource.lineRenderer;
-        // var gunEnd = attackSource.gunEnd;
-        // var weaponRange = attackSource.weaponRange;
 
 
-        float angle = gunPivot.transform.rotation.eulerAngles.z;
+        float angle = attackSource.gunPivot.transform.rotation.eulerAngles.z;
         Quaternion rotationAngle = Quaternion.Euler(0f, 0f, angle);
 
-        Vector2 targetDir = gunPivot.transform.right.normalized;
-        RaycastHit2D hit = Physics2D.Raycast(gunEnd.transform.position, targetDir, weaponRange);
-        lineRenderer.SetPosition(0, gunPivot.transform.position);
+        Vector2 targetDir = attackSource.gunPivot.transform.right.normalized;
+        RaycastHit2D hit = Physics2D.Raycast(attackSource.gunEnd.transform.position, targetDir, attackSource.weaponData.baseAttackRange);
+        attackSource.lineRenderer.SetPosition(0, attackSource.gunPivot.transform.position);
         if (hit.collider != null)
         {
 
 
-            lineRenderer.SetPosition(1, hit.point);
+            attackSource.lineRenderer.SetPosition(1, hit.point);
             var test = hit.collider.transform.root.GetComponent<IAttackable>();
             if (test != null)
             {
-                test.IsAttacked(this.gameObject, this.weaponData);
+                test.IsAttacked(attackSource.gameObject, attackSource.weaponData);
             }
 
 
         }
         else
         {
-            lineRenderer.SetPosition(1, (Vector2)gunEnd.transform.position + targetDir * weaponRange);
+            attackSource.lineRenderer.SetPosition(1, (Vector2)attackSource.gunEnd.transform.position + targetDir * attackSource.weaponData.baseAttackRange);
         }
 
     }
 
 
-    void projectileAttack(Turret attackSource)
+    public static void projectileAttack(Turret attackSource)
     {
         throw new NotImplementedException();
     }
 
-    void areaAttack(Turret attackSource)
+    public static void areaAttack(Turret attackSource)
     {
         throw new NotImplementedException();
     }
@@ -234,11 +181,81 @@ public class Turret : MonoBehaviour
 
 
     #endregion
-
-
-
-
 }
 
 
 
+public static class AutoTurretAimStuff
+{
+        public static Vector3? CalculateAutoAimTarget(Turret _turret)
+    {
+        List<Vehicle> _enemyVehicles = _turret.owner.CalculateEnemyVehicles();
+        List<GameObject> _enemyObjects = new List<GameObject>();
+        foreach (Vehicle vehicle in _enemyVehicles)
+        { _enemyObjects.Add(vehicle.gameObject); }
+
+        List<GameObject> _validTargetsByAngle = ValidTargetsByAngle(_objectsList: _enemyObjects, _turret: _turret);
+
+        GameObject _closestVehicle = ClosestGameObject(_objectsList: _validTargetsByAngle, _turret: _turret);
+        if(_closestVehicle != null)
+        {
+            return _closestVehicle.transform.position;
+        } else {return null;}
+        
+    }
+
+    public static List<GameObject> ValidTargetsByAngle(List<GameObject> _objectsList, Turret _turret)
+    {
+        List<GameObject> _outputList = new List<GameObject>();
+
+        foreach (GameObject _currentObject in _objectsList)
+        {
+            Vector3 _direction = (_currentObject.transform.position - _turret.gunPivot.transform.position).normalized;
+            float targetAngle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
+
+            // Adjust the target angle based on the initial rotation
+            float adjustedTargetAngle = targetAngle - _turret.initialRotation;
+
+            // Normalize the adjusted target angle to be within the range of -180 to 180 degrees
+            if (adjustedTargetAngle > 180f)
+                adjustedTargetAngle -= 360f;
+            else if (adjustedTargetAngle < -180f)
+                adjustedTargetAngle += 360f;
+
+
+
+            if (adjustedTargetAngle < _turret.pivotLimit && adjustedTargetAngle > -_turret.pivotLimit)
+            {
+                _outputList.Add(_currentObject);
+            }
+        }
+
+
+        return _outputList;
+    }
+
+    public static GameObject ClosestGameObject(List<GameObject> _objectsList, Turret _turret)
+    { //this should probably go somewhere else to be honest
+
+    #nullable enable
+        GameObject? closestObject = null;
+    #nullable disable
+        foreach (var _gameObject in _objectsList)
+        {
+            var distanceFromObject = Vector3.Distance(_turret.gameObject.transform.position, _gameObject.transform.position);
+            if (closestObject == null || distanceFromObject < Vector3.Distance(_turret.gameObject.transform.position, closestObject.transform.position))
+            {
+                closestObject = _gameObject;
+            }
+        }
+        if (closestObject != null)
+        {
+            return closestObject;
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+}
